0843ccb5807331b39a49c4864cd16a5f91bd5873
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-09-28 02:48:21 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,6 +11,10 @@ class Post < ActiveRecord::Base
   def self.from_today
     where("created_at >=?", Time.zone.today.beginning_of_day)
   end
+  
+  def self.old_news
+    where("created_at <?", Time.zone.today.beginning_of_day)
+  end
 
   private
 
